a817600d9bace4a76ab51e21035f7ecd7dc9d595
Shayan Saha <shayan851997@gmail.com>
2021-05-29 12:37:05 +0530

    changed src

--- ramit.py
+++ ramit.py
@@ -1,8 +1,22 @@
 import sys
 import time
 from memory_profiler import profile
+import shutil
 
 chunks=100
+def print_centre(s):
+    print(s.center(shutil.get_terminal_size().columns))
+
+
+heading1 = "RAMIT"
+heading2 = "================================================================================================="
+heading3 = "This console application will allow you to consume a given amount of RAM for a given time"
+print()
+print_centre(heading1)
+print_centre(heading2)
+print_centre(heading3)
+print_centre(heading2)
+print()
 endMemory = int(input("Enter RAM (in MB) you want to allocate : "))
 print()
 t = int(input("For how long you want to keep? (in sec) : "))
